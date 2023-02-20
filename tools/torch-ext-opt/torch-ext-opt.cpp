338c5274d38846b22722005641ac2bd841775b80
gilsaia <794433219@qq.com>
2023-02-20 18:15:24 +0800

    fix cmake

--- tools/torch-ext-opt/torch-ext-opt.cpp
+++ tools/torch-ext-opt/torch-ext-opt.cpp
@@ -1,6 +1,12 @@
 #include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "torch-mlir-dialects/Dialect/TMTensor/IR/TMTensorDialect.h"
+#include "torch-mlir/Conversion/Passes.h"
+#include "torch-mlir/Dialect/Torch/IR/TorchDialect.h"
+#include "torch-mlir/Dialect/Torch/Transforms/Passes.h"
+#include "torch-mlir/Dialect/TorchConversion/IR/TorchConversionDialect.h"
+#include "torch-mlir/Dialect/TorchConversion/Transforms/Passes.h"
 #include "torch-mlir/InitAll.h"
 
 using namespace mlir;
@@ -8,10 +14,16 @@ using namespace mlir;
 int main(int argc, char **argv) {
   registerAllPasses();
   //   mlir::torch::registerAllPasses();
+  mlir::torch::registerTorchPasses();
+  mlir::torch::registerTorchConversionPasses();
+  mlir::torch::registerConversionPasses();
 
   DialectRegistry registry;
   registerAllDialects(registry);
-  //   mlir::torch::registerAllDialects(registry);
+  registry.insert<mlir::torch::Torch::TorchDialect>();
+  registry.insert<mlir::torch::TorchConversion::TorchConversionDialect>();
+  registry.insert<mlir::torch::TMTensor::TMTensorDialect>();
+  // mlir::torch::registerAllDialects(registry);
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "MLIR modular optimizer driver\n", registry,
