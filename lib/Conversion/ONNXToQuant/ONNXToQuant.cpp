c0a84eaacba0c290b5da634864d5415a5bbde485
gilsaia <794433219@qq.com>
2023-03-06 22:30:29 -0500

    add onnxtoquant

--- lib/Conversion/ONNXToQuant/ONNXToQuant.cpp
+++ lib/Conversion/ONNXToQuant/ONNXToQuant.cpp
@@ -1,6 +1,56 @@
+#include "mlir/Dialect/Quant/QuantOps.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
+
 #include "quant-ext-opt/Conversion/ONNXToQuant/ONNXToQuant.h"
+#include "src/Dialect/ONNX/ONNXOps.hpp"
 
 #include "../PassDetail.h"
 
-std::unique_ptr<OperationPass<func::FuncOp>>
-mlir::quant_ext::createConvertONNXToQuantPass() {}
+namespace {
+class ConvertQuantizeLinearOp
+    : public mlir::OpConversionPattern<mlir::ONNXQuantizeLinearOp> {
+public:
+  using OpConversionPattern::OpConversionPattern;
+  mlir::LogicalResult
+  matchAndRewrite(mlir::ONNXQuantizeLinearOp op,
+                  mlir::ONNXQuantizeLinearOpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    rewriter.replaceOpWithNewOp<mlir::quant::QuantizeCastOp>(
+        op, getTypeConverter()->convertType(op.getType()), adaptor.getX());
+    return mlir::success();
+  }
+};
+} // namespace
+
+namespace {
+class ConvertONNXToQuant
+    : public impl::ConvertONNXToQuantBase<ConvertONNXToQuant> {
+  void getDependentDialects(mlir::DialectRegistry &registry) const override {
+    registry.insert<mlir::ONNXDialect>();
+    registry.insert<mlir::quant::QuantizationDialect>();
+  }
+  void runOnOperation() override {
+    mlir::MLIRContext *context = &getContext();
+    mlir::ConversionTarget target(*context);
+    target
+        .addLegalDialect<mlir::ONNXDialect, mlir::quant::QuantizationDialect>();
+
+    mlir::TypeConverter typeConverter;
+    typeConverter.addConversion([](mlir::Type type) { return type; });
+
+    mlir::RewritePatternSet patterns(context);
+    target.addIllegalOp<mlir::ONNXQuantizeLinearOp>();
+    patterns.add<ConvertQuantizeLinearOp>(typeConverter, context);
+    if (failed(applyPartialConversion(getOperation(), target,
+                                      std::move(patterns)))) {
+      return signalPassFailure();
+    }
+  }
+};
+} // namespace
+
+std::unique_ptr<mlir::OperationPass<mlir::func::FuncOp>>
+mlir::quant_ext::createConvertONNXToQuantPass() {
+  return std::make_unique<ConvertONNXToQuant>();
+}
--- lib/Conversion/PassDetail.h
+++ lib/Conversion/PassDetail.h
@@ -2,14 +2,10 @@
 #define QUANTEXTMLIR_CONVERSION_PASSDETAIL_H
 
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/MLIRContext.h"
 #include "mlir/Pass/Pass.h"
 
-namesapce mlir {
-  namesapce quant_ext {
-
-#define GEN_PASS_DECL
+#define GEN_PASS_DEF_CONVERTONNXTOQUANT
 #include "quant-ext-opt/Conversion/Passes.h.inc"
-  }
-}
 
 #endif
