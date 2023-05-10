bf5ec0cb050c639d755895aa3aaf64fac98f62f7
gilsaia <794433219@qq.com>
2023-05-10 15:26:51 +0800

    feat:complete init lower pipe

--- lib/Pipeline/LowerTorchToLLVMIR.cpp
+++ lib/Pipeline/LowerTorchToLLVMIR.cpp
@@ -1,11 +1,23 @@
+#include "mlir/Conversion/AffineToStandard/AffineToStandard.h"
+#include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
+#include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
 #include "mlir/Conversion/LinalgToLLVM/LinalgToLLVM.h"
+#include "mlir/Conversion/Passes.h"
+#include "mlir/Conversion/ReconcileUnrealizedCasts/ReconcileUnrealizedCasts.h"
+#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
 #include "mlir/Conversion/TosaToArith/TosaToArith.h"
 #include "mlir/Conversion/TosaToLinalg/TosaToLinalg.h"
+#include "mlir/Conversion/VectorToLLVM/ConvertVectorToLLVM.h"
+#include "mlir/Conversion/VectorToSCF/VectorToSCF.h"
+#include "mlir/Dialect/Arith/Transforms/Passes.h"
 #include "mlir/Dialect/Bufferization/Transforms/Passes.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Func/Transforms/Passes.h"
 #include "mlir/Dialect/Linalg/Passes.h"
 #include "mlir/Dialect/MemRef/Transforms/Passes.h"
 #include "mlir/Dialect/SCF/Transforms/Passes.h"
+#include "mlir/Dialect/Tensor/Transforms/Passes.h"
+#include "mlir/Dialect/Tosa/Transforms/Passes.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Transforms/Passes.h"
@@ -17,19 +29,32 @@
 using namespace mlir;
 void quant_ext::createTorchToLLVMIRPipeline(OpPassManager &pm) {
   torch::TorchConversion::createTorchBackendToTosaBackendPipeline(pm);
-  tosa::addTosaToLinalgPasses(pm, false);
+  pm.addPass(tosa::createTosaLayerwiseConstantFoldPass());
+  pm.addPass(tosa::createTosaMakeBroadcastablePass());
   pm.addPass(tosa::createTosaToArith(true, true));
-  pm.addPass(createCanonicalizerPass());
-  pm.addPass(createCSEPass());
+  tosa::addTosaToLinalgPasses(pm, false);
+  pm.addNestedPass<func::FuncOp>(createCanonicalizerPass());
+  pm.addNestedPass<func::FuncOp>(createCSEPass());
   pm.addPass(bufferization::createEmptyTensorToAllocTensorPass());
-  pm.addPass(bufferization::createOneShotBufferizePass());
-  pm.addPass(createCanonicalizerPass());
-  pm.addPass(createCSEPass());
+  pm.addPass(createLinalgBufferizePass());
+  pm.addPass(createSCFBufferizePass());
+  pm.addPass(arith::createArithBufferizePass());
+  pm.addPass(createTensorBufferizePass());
+  pm.addPass(func::createFuncBufferizePass());
+  pm.addPass(bufferization::createFinalizingBufferizePass());
+  // pm.addPass(bufferization::createOneShotBufferizePass());
+  pm.addNestedPass<func::FuncOp>(createCanonicalizerPass());
+  pm.addNestedPass<func::FuncOp>(createCSEPass());
+  // pm.addNestedPass<func::FuncOp>(bufferization::createBufferDeallocationPass());
+  // pm.addNestedPass<func::FuncOp>(createConvertVectorToSCFPass());
+  pm.addNestedPass<func::FuncOp>(createConvertLinalgToLoopsPass());
+  // pm.addNestedPass<func::FuncOp>(createCanonicalizerPass());
+  pm.addNestedPass<func::FuncOp>(createConvertSCFToCFPass());
+  pm.addPass(createConvertLinalgToLLVMPass());
   pm.addPass(memref::createExpandStridedMetadataPass());
-  pm.addPass(memref::createExpandOpsPass());
-  pm.addPass(createConvertLinalgToParallelLoopsPass());
-  pm.addPass(createParallelLoopFusionPass());
-  pm.addPass(createCanonicalizerPass());
-  pm.addPass(createCSEPass());
-  // pm.addPass(createConvertLinalgToLLVMPass());
+  pm.addPass(createArithToLLVMConversionPass());
+  pm.addNestedPass<func::FuncOp>(createConvertSCFToCFPass());
+  pm.addPass(createFinalizeMemRefToLLVMConversionPass());
+  pm.addPass(createConvertFuncToLLVMPass());
+  pm.addPass(createReconcileUnrealizedCastsPass());
 }
